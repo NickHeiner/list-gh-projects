2edb30af23daf4ebb62d61afc41ae2165927cafd
Nick Heiner <nth23@cornell.edu>
2018-04-09 18:43:52 -0700

    Cleaning up results page more

--- src/ResultsPage.js
+++ src/ResultsPage.js
@@ -14,6 +14,7 @@ const REQUEST_STATUS = {
 
 const BareList = ({children}) => {
   const styles = css({
+    paddingLeft: 0,
     listStyle: 'none'
   });
   return <ul {...styles}>{children}</ul>;
@@ -23,11 +24,18 @@ const CommitResultItem = ({commit}) => <p>{commit.messageHeadline}</p>;
 
 const RepoResultItem = ({repo}) => {
   const {name, forks, stargazers, defaultBranchRef, url} = repo;
+
+  const rootStyles = css({
+    border: '1px rgb(234, 236, 239) solid',
+    marginBottom: '10px',
+    padding: '5px'
+  });
   const headerRowStyles = css({
     display: 'flex',
     alignItems: 'baseline'
   });
   const headerStyles = css({
+    marginTop: 0,
     marginRight: '10px'
   });
   const linkStyles = css({
@@ -41,12 +49,16 @@ const RepoResultItem = ({repo}) => {
     color: '#586069',
     fontSize: '.85rem'
   });
-  return <div>
+  const commitsHeaderStyles = css({
+    marginTop: 0,
+    marginBottom: 0
+  });
+  return <div {...rootStyles}>
     <div {...headerRowStyles}>
       <h2 {...headerStyles}><a href={url} {...linkStyles}>{name}</a></h2>
       <p {...subtitleStyles}>({forks.totalCount} forks; {stargazers.totalCount} stars)</p>
     </div>
-    <h3>Commits</h3>
+    <h3 {...commitsHeaderStyles}>Commits</h3>
     <BareList>
       {
         defaultBranchRef.target.history.nodes.map(
